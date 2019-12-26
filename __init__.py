0b2b9cf61548a03ec4b2d763ac313983495c4f84
chancel <ycs1026@vip.qq.com>
2019-12-26 10:13:22 +0800

    [imp] 添加配置文件

--- __init__.py
+++ __init__.py
@@ -2,35 +2,14 @@
 # author - chancel.yang
 import urllib.request
 import os
+import sys
 import time
 from bs4 import BeautifulSoup
 import requests
 import json
+from collections import namedtuple
 
-nginx_url = "http://127.0.0.1:9092/"
-aria2_url = "http://127.0.0.1:6800/jsonrpc"
-restrict_download_format = {
-    "enable": True,
-    "format": ["mp4", "mkv", "rmvb", "flv", "avi", "mov", "mpg"]
-}
-
-
-def get_href(url):
-    """Get Webpage all href"""
-    html = urllib.request.urlopen(url).read().decode("utf-8")
-    soup = BeautifulSoup(html, features='html.parser')
-    tags = soup.find_all('a')
-    _hrefs = []
-    for tag in tags:
-        href = tag.get('href').strip()
-        if href == "../":
-            continue
-        if href[-1] == "/":
-            _hrefs.extend(get_href(url+href))
-            continue
-        _hrefs.append(url+href)
-    return _hrefs
-
+config_object = None
 
 def get_local_record():
     """Read download record by local file"""
@@ -44,15 +23,31 @@ def get_local_record():
     return records
 
 
+def commit_download_task(url):
+    """Create download task on aria2"""
+    _accept = 'application/json, text/plain, */*'
+    _content_type = 'application/json;charset=UTF-8'
+    _user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'
+    params = [[url]]
+    if config_object.download_path is not None:
+        file_path = config_object.download_path + \
+            '/'.join(x for x in (url.split('/')[1:-1]))
+        params.append({"dir": file_path})
+    data = {'id': str(time.time()), 'jsonrpc': '2.0', "method": "aria2.addUri",
+            "params": params}
+    response = requests.post(config_object.aria2_rpc_url, json.dumps(data))
+    return response.ok
+
+
 def analyze_url(url):
     """Analyze url"""
     records = get_local_record()
     for _item in records:
         if _item == url + "\n":
             return
-    if restrict_download_format["enable"] is True:
+    if config_object.disable_file_type.enable is True:
         _file_format = url.split(".")[-1]
-        if _file_format not in restrict_download_format["format"]:
+        if _file_format in config_object.disable_file_type.formats:
             return
     commit_result = commit_download_task(url)
     if commit_result:
@@ -62,18 +57,28 @@ def analyze_url(url):
             f.write(url + '\n')
 
 
-def commit_download_task(url):
-    """Create download task on aria2"""
-    _accept = 'application/json, text/plain, */*'
-    _content_type = 'application/json;charset=UTF-8'
-    _user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'
-    data = {'id': str(time.time()), 'jsonrpc': '2.0', "method": "aria2.addUri",
-            "params": [[url]]}
-    response = requests.post(aria2_url, json.dumps(data))
-    return response.ok
+def get_href_by_nginx(url):
+    """Get nginx webpage all href"""
+    html = urllib.request.urlopen(url).read().decode("utf-8")
+    soup = BeautifulSoup(html, features='html.parser')
+    tags = soup.find_all('a')
+    _hrefs = []
+    for tag in tags:
+        href = tag.get('href').strip()
+        if href == "../":
+            continue
+        if href[-1] == "/":
+            _hrefs.extend(get_href_by_nginx(url+href))
+            continue
+        _hrefs.append(url+href)
+    return _hrefs
 
 
 if __name__ == "__main__":
-    hrefs = get_href(nginx_url)
-    for href in hrefs:
-        analyze_url(href)
+    with open(os.path.join(sys.path[0],'config.json'),'r') as f:
+        config_object = f.read()
+    config_object = json.loads(config_object, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+    for nginx_url_item in config_object.nginx_url:
+        hrefs = get_href_by_nginx(nginx_url_item)
+        for href in hrefs:
+            analyze_url(href)
